840634e8e260808351133126a064ca1f3fc1d9ff
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-10-13 16:15:11 -0700

    Integrate Redux

---
+++ src/redux/configure-store.js
@@ -0,0 +1,20 @@
+import {
+  applyMiddleware,
+  createStore,
+} from 'redux'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import rootReducer from './reducers'
+import initialState from './initial-state'
+
+const configureStore = () => {
+  return createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(
+      applyMiddleware(thunk),
+    ),
+  )
+}
+
+export default configureStore

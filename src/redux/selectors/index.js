b814ec5bf353e60adbd8be984f7c623b8ad726b5
Kyle Corbelli <kyle.corbelli@gmail.com>
2017-10-15 06:14:27 -0700

    Unit test Redux selectors

--- src/redux/selectors/index.js
+++ src/redux/selectors/index.js
@@ -1,14 +1,12 @@
-export const byIdToArray = (byId) => Object.keys(byId).map(id => byId[id])
-
-export const orderedProductsByLayer = (products) => {
+export const orderedProductsByIndex = (products) => {
   return products.slice().sort((a, b) => a.index - b.index)
 }
 
 export const selectedProducts = (state) => {
   const { byId, idsSelected } = state.products
-  const selectedProductIds = byIdToArray(idsSelected)
+  const selectedProductIds = Object.values(idsSelected)
   const selectedProducts = selectedProductIds.map(id => byId[id])
-  return orderedProductsByLayer(selectedProducts)
+  return orderedProductsByIndex(selectedProducts)
 }
 
 export const productTypeInFocus = (state) => {
@@ -19,7 +17,7 @@ export const productTypeInFocus = (state) => {
 export const similarProducts = (state) => {
   const { byId } = state.products
   const typeInFocus = productTypeInFocus(state)
-  const allProducts = byIdToArray(byId)
-  const sortedProducts = orderedProductsByLayer(allProducts)
+  const allProducts = Object.values(byId)
+  const sortedProducts = orderedProductsByIndex(allProducts)
   return sortedProducts.filter(product => product.type === typeInFocus)
 }
